8f304cd824a2176eb69b9a2212eba583f1e739c5
adityarise2 <137521434+adityarise2@users.noreply.github.com>
2023-08-04 19:05:16 +0530

    Script.py

--- Script.py
+++ Script.py
@@ -1,8 +1,8 @@
 class script(object):
-    START_TXT = """<b>ʜᴇʟᴏ {}
+    START_TXT = """<b>HELLO {}
     
-ɪ ᴀᴍ ᴘᴏᴡᴇʀ ғᴜʟʟ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ʙᴏᴛ
-ɪ ᴀᴍ created by aditya
+ɪ ᴀᴍ ᴘᴏᴡᴇʀ ғᴜʟʟ Movie Search  ʙᴏᴛ
+i Have Over 1Lakh + Movie Files But Not Theatre Prints So Dont Search Theatre Prints 😉
 
 ᴍᴀɴᴛᴀɪɴᴇᴅ ʙʏ : <a href="https://t.me/ajbackupp">ADITYA</a></b> 😏</b>"""
 
