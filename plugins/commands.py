3a260ca7b7f26af5f9997fb42d8b24e20ebb5126
Harshii03 <139242152+Harshii03@users.noreply.github.com>
2023-09-11 01:36:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,8 +23,6 @@ BATCH_FILES = {}
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[
-                    InlineKeyboardButton('⤬ Add Me To Your Group ⤬', url=f'http://t.me/lord_harrypotter_bot?startgroup=true')
-                ],[
                     InlineKeyboardButton('👻 ᴍᴏᴠɪᴇ ᴄʜᴀɴɴᴇʟ', url="https://t.me/ajbackupp"),
                     InlineKeyboardButton('🫨 ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ', url="https://t.me/+u9dSPxCw_LRlODc1")
                 ],[
@@ -38,9 +36,11 @@ async def start(client, message):
             await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
             await db.add_chat(message.chat.id, message.chat.title)
         return 
+
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
+
     if len(message.command) != 2:
         buttons = [[
                     InlineKeyboardButton('⤬ Add Me To Your Group ⤬', url=f'http://t.me/lord_harrypotter_bot?startgroup=true')
