094950bac17b96e3cbbe54bfdcf2345e3e738f46
Harshii03 <139242152+Harshii03@users.noreply.github.com>
2023-09-12 03:28:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -96,4 +96,4 @@ LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies 
 LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
-FILE_REQ_CHANNEL = int(environ.get('FILE_REQ_CHANNEL', '-1001925461912'))
+
